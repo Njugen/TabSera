f97e9413fd2bf95504fb76bd99ee569ae4cea7b6
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2023-10-19 21:53:08 +0300

    updated switcher test

--- src/__tests__/components/utils/switcher_test.tsx
+++ src/__tests__/components/utils/switcher_test.tsx
@@ -1,8 +1,6 @@
-import { render, screen, within, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'
-import PrimaryButton from "../../../components/utils/primary_button";
 import Switcher from "../../../components/utils/switcher";
-import userEvent from "@testing-library/user-event";
 
 const mockFunction = jest.fn(data => data);
 const mockText = "hello mah boy";
@@ -54,25 +52,16 @@ describe("test <PrimaryButton />", () => {
         expect(mockFunction).toHaveBeenCalledWith(false);
     });
 
-    test("clicking works (default value: true, no label)", () => {
-        
-        render(<Switcher value={true} onCallback={mockFunction} />);
+    test.each([true, false])(
+        "renders correctly (default value: %p, no label)", (arg) => {
+            render(<Switcher value={arg} onCallback={mockFunction} />);
 
-        const button = screen.getByRole("button");
-        const text = screen.queryByText(mockText);
+            const button = screen.getByRole("button");
+            const text = screen.queryByText(mockText);
 
-        expect(button).toBeInTheDocument();
-        expect(text).not.toBeInTheDocument();
-    });
-
-    test("clicking works (default value: false, no label)", () => {
-        
-        render(<Switcher value={false} onCallback={mockFunction} />);
-
-        const button = screen.getByRole("button");
-        const text = screen.queryByText(mockText);
-
-        expect(button).toBeInTheDocument();
-        expect(text).not.toBeInTheDocument();
-    });
+            expect(button).toBeInTheDocument();
+            expect(text).not.toBeInTheDocument();
+        }
+    )
+ 
 });
